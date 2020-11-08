a8f0e8c88c4d141347b513b2c101ead23a46f2ec
Roberto Sartori <roberto.sartori.android@gmail.com>
2020-11-08 02:02:06 +0100

    config: add 'fake' properties to pass SafetyNet
    Set the build fingerprint and the Android security patch version to the
    latest official versions from OnePlus for the OnePlus 5.
    Only one issue: on the Android Settings app, the security patch version
    is now wrongly reported.
    
    To pass SafetyNet, it is also necessary to lock the bootloader.
    
    Signed-off-by: Roberto Sartori <roberto.sartori.android@gmail.com>

--- config/BoardConfigAOSP.mk
+++ config/BoardConfigAOSP.mk
@@ -2,3 +2,6 @@ include vendor/rs/config/BoardConfigKernel.mk
 include vendor/rs/config/BoardConfigSoong.mk
 include vendor/rs/config/BoardConfigQcom.mk
 include vendor/rs/build/core/pathmap.mk
+
+TARGET_SYSTEM_PROP += \
+    vendor/rs/config/system.prop
