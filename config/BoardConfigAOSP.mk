2ab6266601aa65c4ea7beb5424a8915eb6b91f0b
Roberto Sartori <roberto.sartori.android@gmail.com>
2020-11-13 22:58:12 +0100

    config: keylayout: add tri-state slider management
    This is an hack needed for the KeyHandler app.
    To use the slider with the screen off, we have to be able to turn on the
    device when the slider changes: to do this, map those 'buttons' as DPAD
    buttos because Android can wake up if a DPAD is used.
    
    Signed-off-by: Roberto Sartori <roberto.sartori.android@gmail.com>

--- config/BoardConfigAOSP.mk
+++ config/BoardConfigAOSP.mk
@@ -5,3 +5,7 @@ include vendor/rs/build/core/pathmap.mk
 
 TARGET_SYSTEM_PROP += \
     vendor/rs/config/system.prop
+
+# Tri-state-key layout
+PRODUCT_COPY_FILES += \
+    vendor/rs/config/keylayout/tri-state-key.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/tri-state-key.kl
