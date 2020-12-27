71e7313ff1ffec87c670114b6e1ba3df9518ce2c
Roberto Sartori <roberto.sartori.android@gmail.com>
2020-12-27 01:04:53 +0100

    config: BoardConfig: include the SEPolicy from Lineage
    Signed-off-by: Roberto Sartori <roberto.sartori.android@gmail.com>

--- config/BoardConfigAOSP.mk
+++ config/BoardConfigAOSP.mk
@@ -17,6 +17,7 @@ PRODUCT_COPY_FILES += \
     vendor/rs/config/permissions/privapp_whitelist_com.android.wallpaper.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp_whitelist_com.android.wallpaper.xml \
     vendor/rs/config/permissions/default_com.android.wallpaper.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/default-permissions/default_com.android.wallpaper.xml
 
+include device/lineage/sepolicy/common/sepolicy.mk
 BOARD_SEPOLICY_DIRS += vendor/rs/sepolicy
 
 SKIP_BOOT_JARS_CHECK := true
