23385919ae7626119cb9bb133d89fc318814c653
Roberto Sartori <roberto.sartori.android@gmail.com>
2021-03-06 01:46:44 +0100

    build: task: kernel: change the KERNEL_CLANG_TRIPLE
    Use the prebuilt compiler to build Android 12.
    
    Signed-off-by: Roberto Sartori <roberto.sartori.android@gmail.com>

--- build/tasks/kernel.mk
+++ build/tasks/kernel.mk
@@ -187,7 +187,7 @@ ifeq ($(TARGET_KERNEL_CLANG_COMPILE),true)
     endif
     TARGET_KERNEL_CLANG_PATH ?= $(BUILD_TOP)/prebuilts/clang/host/$(HOST_OS)-x86/$(KERNEL_CLANG_VERSION)
     ifeq ($(KERNEL_ARCH),arm64)
-        KERNEL_CLANG_TRIPLE ?= CLANG_TRIPLE=aarch64-linux-gnu-
+        KERNEL_CLANG_TRIPLE ?= CLANG_TRIPLE=aarch64-linux-
     else ifeq ($(KERNEL_ARCH),arm)
         KERNEL_CLANG_TRIPLE ?= CLANG_TRIPLE=arm-linux-gnu-
     else ifeq ($(KERNEL_ARCH),x86)
