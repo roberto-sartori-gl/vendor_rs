8144b8c5a649da7d0e5d0cb3b3855eb21a01536f
Roberto Sartori <roberto.sartori.android@gmail.com>
2021-03-27 14:33:06 +0100

    SettingsExtra: vibration: set correct sysfs node for Lazy kernel
    Also, manage the minimum vibration level.
    
    Signed-off-by: Roberto Sartori <roberto.sartori.android@gmail.com>

--- packages/SettingsExtra/src/org/robertogl/settingsextra/Utils.java
+++ packages/SettingsExtra/src/org/robertogl/settingsextra/Utils.java
@@ -174,9 +174,11 @@ public final class Utils {
 
     protected static void setVibrationIntensity(String value, Context context){
         int max = 3596;
+        int min = 116;
         int f_value = (max / 100) * Integer.valueOf(value);
+        if (f_value < min) f_value = min + 1;
         if (DEBUG) Log.d (TAG, "vibration intensity: " + f_value);
-        writeToFile("/sys/devices/virtual/timed_output/vibrator/vmax_mv", String.valueOf(f_value), context);
+        writeToFile("/sys/devices/virtual/timed_output/vibrator/vtg_level", String.valueOf(f_value), context);
     }
 
     public static <T> int[] indexOfMultiple(List<T> list, T object) {
