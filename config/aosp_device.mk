cb6d618815e4412674cf1569bb64b1ab9e0ffa39
Roberto Sartori <roberto.sartori.android@gmail.com>
2021-12-26 11:42:26 +0100

    config: add sysconfig file - fix alarms not working with AOSP Clock app
    For now, it only grants the AOSP Clock to wake up the phone from deep
    sleep in case an alarm is present.
    Fixes alarms not working with AOSP Clock app.
    
    Signed-off-by: Roberto Sartori <roberto.sartori.android@gmail.com>

--- config/aosp_device.mk
+++ config/aosp_device.mk
@@ -40,6 +40,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.4.vendor
 
+# Special permissions for system apps
+PRODUCT_PACKAGES += \
+    aosp-sysconfig.xml
+
 PRODUCT_ENFORCE_RRO_TARGETS := framework-res
 
 PRODUCT_VENDOR_KERNEL_HEADERS := vendor/rs/kernel-headers
