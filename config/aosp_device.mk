16f26bc52831582b77f4c06291e774da4756c801
Roberto Sartori <roberto.sartori.android@gmail.com>
2022-02-17 20:17:58 +0100

    config: remove gsi_keys.mk inclusion
    Not needed on our device.
    
    Signed-off-by: Roberto Sartori <roberto.sartori.android@gmail.com>

--- config/aosp_device.mk
+++ config/aosp_device.mk
@@ -61,6 +61,3 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.sys.binary_xml=false
 
 PRODUCT_BROKEN_VERIFY_USES_LIBRARIES := true
-
-# Installs gsi keys into ramdisk, to boot a GSI with verified boot.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
