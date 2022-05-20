90b8a97019e7248fa6f7c0481ed2a302f382bf12
Maitreya29 <Maitreyapatni30@gmail.com>
2022-05-20 12:52:51 +0200

    config: import kernel headers

--- config/Android.mk
+++ config/Android.mk
@@ -2,3 +2,8 @@ include $(CLEAR_VARS)
 
 # AOSP wlan directory
 include hardware/qcom/wlan/Android.mk
+
+$(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr: $(wildcard $(PRODUCT_VENDOR_KERNEL_HEADERS)/*)
+	rm -rf $@
+	mkdir -p $@/include
+	cp -a $(PRODUCT_VENDOR_KERNEL_HEADERS)/. $@/include
