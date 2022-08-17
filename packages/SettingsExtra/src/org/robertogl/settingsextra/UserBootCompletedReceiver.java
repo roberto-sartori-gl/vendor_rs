02275e0666b314137f7bd17aa215c50bf18ab956
Roberto Sartori <roberto.sartori.android@gmail.com>
2022-08-17 23:17:29 +0200

    Revert "SettingsExtra: enable data and wifi separate tiles"
    This reverts commit 44824cf2f9cc7d73d79ea848a0bed149fd18d92d.
    
    Not working anymore on Android 13.
    
    Signed-off-by: Roberto Sartori <roberto.sartori.android@gmail.com>

--- packages/SettingsExtra/src/org/robertogl/settingsextra/UserBootCompletedReceiver.java
+++ packages/SettingsExtra/src/org/robertogl/settingsextra/UserBootCompletedReceiver.java
@@ -6,7 +6,6 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Handler;
 import android.os.Looper;
-import android.provider.Settings;
 import android.util.Log;
 
 public class UserBootCompletedReceiver extends BroadcastReceiver {
@@ -53,23 +52,10 @@ public class UserBootCompletedReceiver extends BroadcastReceiver {
 
         // Disable volume and alarm AOSP icons
         // We will use our own
-        Utils.removeUnwantendStatusBarIcon(deviceProtectedContext, "volume");
-        Utils.removeUnwantendStatusBarIcon(deviceProtectedContext, "alarm_clock");
+        Utils.removeUnwantendStatusBarIcon(context, "volume");
+        Utils.removeUnwantendStatusBarIcon(context, "alarm_clock");
 
         // Remove rotate icon: we don't need it
-        Utils.removeUnwantendStatusBarIcon(deviceProtectedContext, "rotate");
-
-        // Add back wifi and cell network tile
-        String currentTiles = Settings.Secure.getString(deviceProtectedContext.getContentResolver(), Settings.Secure.QS_TILES);
-        if (!currentTiles.contains("wifi")) {
-            Settings.Secure.putString(deviceProtectedContext.getContentResolver(),
-                Settings.Secure.QS_TILES,currentTiles + ",wifi");
-        }
-
-        currentTiles = Settings.Secure.getString(deviceProtectedContext.getContentResolver(), Settings.Secure.QS_TILES);
-        if (!currentTiles.contains("cell")) {
-            Settings.Secure.putString(deviceProtectedContext.getContentResolver(),
-                Settings.Secure.QS_TILES,currentTiles + ",cell");
-        }
+        Utils.removeUnwantendStatusBarIcon(context, "rotate");
     }
 }
