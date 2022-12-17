eaa1d19260ef2d2392a1863c5ceea4c9dad6c1e2
Roberto Sartori <roberto.sartori.android@gmail.com>
2022-12-17 23:22:40 +0100

    build: task: rename ota_norecovery to ota_custom
    Now, this task does more than only removing the recovery update (it also
    removes the date check for downgrades during OTA updates), so change its
    name.
    
    Signed-off-by: Roberto Sartori <roberto.sartori.android@gmail.com>

--- build/tasks/ota_custom.mk
+++ build/tasks/ota_custom.mk
@@ -9,13 +9,13 @@ target_files_name := $(name)-target_files-$(FILE_NAME_TAG)
 ota_name := $(name)-ota-$(FILE_NAME_TAG)
 target_files_zip_root := $(intermediates)/$(target_files_name)
 
-INTERNAL_OTA_PACKAGE_TARGET_NO_RECOVERY := $(PRODUCT_OUT)/$(ota_name)-no_recovery.zip
-INTERNAL_OTA_METADATA_NO_RECOVERY := $(PRODUCT_OUT)/ota_metadata_no_recovery
+INTERNAL_OTA_PACKAGE_TARGET_CUSTOM := $(PRODUCT_OUT)/$(name)-ota.zip
+INTERNAL_OTA_METADATA_CUSTOM := $(PRODUCT_OUT)/ota_metadata_custom
 
-$(INTERNAL_OTA_PACKAGE_TARGET_NO_RECOVERY): KEY_CERT_PAIR := $(DEFAULT_KEY_CERT_PAIR)
-$(INTERNAL_OTA_PACKAGE_TARGET_NO_RECOVERY): .KATI_IMPLICIT_OUTPUTS := $(INTERNAL_OTA_METADATA_NO_RECOVERY)
-$(INTERNAL_OTA_PACKAGE_TARGET_NO_RECOVERY): $(BUILT_TARGET_FILES_PACKAGE) $(OTA_FROM_TARGET_FILES) $(INTERNAL_OTATOOLS_FILES)
-	@echo "Package OTA without recovery: $@"
+$(INTERNAL_OTA_PACKAGE_TARGET_CUSTOM): KEY_CERT_PAIR := $(DEFAULT_KEY_CERT_PAIR)
+$(INTERNAL_OTA_PACKAGE_TARGET_CUSTOM): .KATI_IMPLICIT_OUTPUTS := $(INTERNAL_OTA_METADATA_CUSTOM)
+$(INTERNAL_OTA_PACKAGE_TARGET_CUSTOM): $(BUILT_TARGET_FILES_PACKAGE) $(OTA_FROM_TARGET_FILES) $(INTERNAL_OTATOOLS_FILES)
+	@echo "Package OTA (custom): $@"
 	$(hide) $(HOST_OUT_EXECUTABLES)/sign_target_files_apks -o --default_key_mappings vendor/rs/config/security/ $(BUILT_TARGET_FILES_PACKAGE) $(BUILT_TARGET_FILES_PACKAGE).signed
 	$(hide) mv $(BUILT_TARGET_FILES_PACKAGE).signed $(BUILT_TARGET_FILES_PACKAGE)
 	$(hide) rm -rf $(target_files_zip_root)
@@ -27,7 +27,7 @@ $(INTERNAL_OTA_PACKAGE_TARGET_NO_RECOVERY): $(BUILT_TARGET_FILES_PACKAGE) $(OTA_
 	$(hide) zip -q -d $(BUILT_TARGET_FILES_PACKAGE) 'IMAGES/*'
 	$(hide) $(ADD_IMG_TO_TARGET_FILES) -a $(BUILT_TARGET_FILES_PACKAGE)
 	$(hide) unzip -o -j $(BUILT_TARGET_FILES_PACKAGE) -d $(target_files_zip_root)/IMAGES 'IMAGES/*'
-	$(call build-ota-package-target,$@,-k $(KEY_CERT_PAIR) --output_metadata_path $(INTERNAL_OTA_METADATA_NO_RECOVERY))
+	$(call build-ota-package-target,$@,-k $(KEY_CERT_PAIR) --output_metadata_path $(INTERNAL_OTA_METADATA_CUSTOM))
 	$(hide) mkdir -p $(target_files_zip_root)/OTA_TMP
 	$(hide) unzip -o $@ -d $(target_files_zip_root)/OTA_TMP 'META-INF/com/google/android/updater-script'
 	$(hide) sed -i 1d $(target_files_zip_root)/OTA_TMP/META-INF/com/google/android/updater-script
@@ -36,6 +36,6 @@ $(INTERNAL_OTA_PACKAGE_TARGET_NO_RECOVERY): $(BUILT_TARGET_FILES_PACKAGE) $(OTA_
 	$(hide) java -jar -Djava.library.path="out/host/linux-x86/lib64" $(HOST_OUT_EXECUTABLES)/../framework/signapk.jar -w vendor/rs/config/security/releasekey.x509.pem vendor/rs/config/security/releasekey.pk8 $(target_files_zip_root)/OTA_TMP/tmp.zip $@
 	$(hide) rm -rf $(target_files_zip_root)/OTA_TMP
 
-.PHONY: otapackage_norecovery
-otapackage_norecovery: $(INTERNAL_OTA_PACKAGE_TARGET_NO_RECOVERY)
-	@echo "Package Complete: $(INTERNAL_OTA_PACKAGE_TARGET_NO_RECOVERY)" >&2
+.PHONY: otapackage_custom
+otapackage_custom: $(INTERNAL_OTA_PACKAGE_TARGET_CUSTOM)
+	@echo "Package Complete: $(INTERNAL_OTA_PACKAGE_TARGET_CUSTOM)" >&2
